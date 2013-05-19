923fd87d87fe1a22f04963d491fa2af15c63e143
Aliaksey Kandratsenka <alk@tut.by>
2013-05-18 17:11:58 -0700

    issue-66: use signal's ucontext when unwinding out backtrace
    In issue-66 (and readme) it is pointed out that sometimes there are
    some issues grabbing backtrace across signal handler boundary.
    
    This code attempts to fix it by grabbing backtrace from signal's
    ucontext which clearly does not include signal handler boundary.
    
    We're using "feature" of libunwind that for some important platforms
    libunwind's context is same as libc's ucontext_t which is given to as
    part of calling signal handler.

--- src/stacktrace_libunwind-inl.h
+++ src/stacktrace_libunwind-inl.h
@@ -88,10 +88,27 @@ int GET_STACK_TRACE_OR_FRAMES {
   }
   ++recursive;
 
+#if (IS_WITH_CONTEXT && (defined(__i386__) || defined(__x86_64__)) && defined(__GNU_LIBRARY__))
+  if (ucp) {
+    uc = *(unw_context_t *)ucp;
+    /* this is a bit weird. profiler.cc calls us with signal's ucontext
+     * yet passing us 2 as skip_count and essentially assuming we won't
+     * use ucontext. */
+    /* In order to fix that I'm going to assume that if ucp is
+     * non-null we're asked to ignore skip_count in case we're
+     * able to use ucp */
+    skip_count = 0;
+  } else {
+    unw_getcontext(&uc);
+    skip_count++;         // Do not include current frame
+  }
+#else
   unw_getcontext(&uc);
+  skip_count++;         // Do not include current frame
+#endif
+
   int ret = unw_init_local(&cursor, &uc);
   assert(ret >= 0);
-  skip_count++;         // Do not include current frame
 
   while (skip_count--) {
     if (unw_step(&cursor) <= 0) {
